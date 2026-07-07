2bc17279f30222b72bc4ff1b7f1eaa957f559734
SHIBU RAMACHANDRAN <cyberonecsc@gmail.com>
2026-07-08 01:29:33 +0530

    Configure run.bat to automatically open browser on startup

--- run.bat
+++ run.bat
@@ -1,5 +1,9 @@
 @echo off
 title CYBERONE CSC Local Sync Server
 echo Starting CYBERONE CSC local sync server...
+
+:: Open browser immediately
+start http://localhost:8080/
+
 node "%~dp0server.js"
 pause
